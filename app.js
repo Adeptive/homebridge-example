3879caa32dbe323a74632bf8b9fb695b301bf58b
michaelgeerts <geertsmichael@gmail.com>
2016-01-24 16:48:36 +0100

    added path resolving for persist folder

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var User = require('homebridge/lib/user').User;
+var path = require('path');
 
-User.setStoragePath('./');
+var storagePath = './';
+storagePath = path.resolve(storagePath);
+
+User.setStoragePath(storagePath);
 
 var homebridge = require('homebridge/bin/homebridge');
